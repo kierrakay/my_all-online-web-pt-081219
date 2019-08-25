31859e91772ef44e7451cdca026d3e8b2bee7c50
Kierra M <k.merriman32@hotmail.com>
2019-08-25 19:14:04 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,21 +1,6 @@
 require 'pry'
 
-def my_all?(collection)
-i = 0 
-while i < collection.length 
-block_return_values <<
-yield(collection[i])
-i += 1
-end
-end
-if
-  block_return_values.include?
-  (false)
-  false
-else
-  true 
-end
-end
+
 
 
 
